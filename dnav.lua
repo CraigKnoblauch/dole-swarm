2dc4635f1c65a1ce8b8873996609fe899a6f520b
Craig Knoblauch <craigknoblauch@gmail.com>
2018-12-20 17:46:43 -0500

    using metatables

--- dnav.lua
+++ dnav.lua
@@ -4,12 +4,8 @@
 -- as providing navigational capabilities. 
 -- @classmod DNav
 -- @author Craig Knoblauch
-DNav = {
-    x, -- int: x position of turtle
-    y, -- int: y position of turtle
-    z, -- int: z position of turtle
-    facing -- string: Cardinal direction the turtle is facing
-}
+local DNav = {}
+DNav.__index = DNav
 
 ---
 -- Creates and returns a DNav object
@@ -19,7 +15,9 @@ DNav = {
 -- @param facing The turtle's starting cardinal direction
 -- @return A new DNav object
 -- @todo Error checking
-function DNav:new(x, y, z, facing)
+function DNav.new(x, y, z, facing)
+    local self = setmetatable({}, DNav)
+
     self.x = x
     self.y = y
     self.z = z
@@ -29,13 +27,13 @@ function DNav:new(x, y, z, facing)
 end
 
 -- NOTE For testing purposes only
-my_nav = DNav:new(1, 2, 3, "NORTH")
+my_nav = DNav.new(1, 2, 3, "NORTH")
 print("x: ".(my_nav.x))
 print("y: ".(my_nav.y))
 print("z: ".(my_nav.z))
 print("facing: ".(my_nav.facing))
 
-my_other_nav = DNav:new(-1, -2, -3, "SOUTH")
+my_other_nav = DNav.new(-1, -2, -3, "SOUTH")
 print("x: ".(my_nav.x))
 print("y: ".(my_nav.y))
 print("z: ".(my_nav.z))
